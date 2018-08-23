50a70557b1df7e865165c2a7cf326ae64933c049
AJ Larson <aj.larson@devmounta.in>
2018-08-23 16:27:15 -0600

    initial commit

---
+++ functions/data/employees.js
@@ -0,0 +1,31 @@
+module.exports = [
+  {
+    id: 9990,
+    name: 'Han Solo',
+    phone: '8015551234',
+    email: 'solo@sabaac.com',
+    title: 'Smuggler'
+  },
+  {
+    id: 9991,
+    name: 'Chewbacca',
+    phone: '8015551235',
+    email: 'copilot@falcon.com',
+    title: 'Lhuorwo Sorroohraanorworc'
+  },
+  {
+    id: 9992,
+    name: 'Luke Skywalker',
+    phone: '8015551236',
+    email: 'master@jedi.org',
+    title: 'Not My Father'
+  },
+  {
+    id: 9993,
+    name: 'Leia Organa',
+    phone: '8015551237',
+    email: 'general@resistance.mil',
+    title: 'General / Highness'
+  }
+
+]
