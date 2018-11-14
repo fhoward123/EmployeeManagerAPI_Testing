1206481e88105fc538ef7028c64d3c4b24571797
DevMountain QA <aj.larson@devmounta.in>
2018-11-13 18:07:15 -0700

    Delete nightwatch.props.js

--- nightwatch.props.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    resourcePath: "/Users/ajlarson/src/testing-resources/selenium-resources/",
-    seleniumServer: "selenium-server-standalone-3.12.0.jar",
-    chromedriver: "chromedriver"
- }
